f9690fd6353e82bff8ac4ae27ec3035f599fae96
PeterfiZ <peterfi.zoltan@pte.hu>
2025-10-24 09:57:41 +0200

    Add files via upload

---
+++ continents.js
@@ -0,0 +1,44 @@
+const continents = {
+    "Európa": {
+        "name": "Európa",
+        "emoji": "🇪🇺",
+        "description": "Az európai kontinens országai",
+        "color": "blue"
+    },
+    "Észak-Amerika": {
+        "name": "Észak-Amerika",
+        "emoji": "🦅",
+        "description": "Az észak-amerikai kontinens országai",
+        "color": "green"
+    },
+      "Közép-Amerika": {
+        "name": "Közép-Amerika",
+        "emoji": "🏝️",
+        "description": "A közép-amerikai és karibi országok",
+        "color": "pink"
+    },
+    "Dél-Amerika": {
+        "name": "Dél-Amerika",
+        "emoji": "🦙",
+        "description": "A dél-amerikai kontinens országai",
+        "color": "yellow"
+    },
+    "Afrika": {
+        "name": "Afrika",
+        "emoji": "🐘",
+        "description": "Az afrikai kontinens országai",
+        "color": "red"
+    },
+    "Ázsia": {
+        "name": "Ázsia",
+        "emoji": "🐉",
+        "description": "Az ázsiai kontinens országai",
+        "color": "orange"
+    },
+    "Óceánia": {
+        "name": "Óceánia",
+        "emoji": "🏝️",
+        "description": "Az óceániai kontinens országai",
+        "color": "teal"
+    }
+};
